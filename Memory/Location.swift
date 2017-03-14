b38aa036159ed398bec1da785181b4eb6fd03287
Jason <jawong1212@gmail.com>
2017-03-13 21:41:50 -0700

    init commit

---
+++ Memory/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  Memory
+//
+//  Created by Jason Wong on 3/13/17.
+//  Copyright © 2017 Jason Wong. All rights reserved.
+//
+
+import UIKit
+
+class Location: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Memory/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Memory
+//
+//  Created by Jason Wong on 3/13/17.
+//  Copyright © 2017 Jason Wong. All rights reserved.
+//
+
+import UIKit
+
+class User: UIView {
+    var id : String = ""
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
