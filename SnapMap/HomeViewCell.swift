9e3b76c201aad81166a447d0d3662f5f9232f3fd
Lum Situ <lumsitu@Lums-MacBook-Pro.local>
2017-04-12 18:32:40 -0700

    Working on the HomeView

---
+++ SnapMap/HomeViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewCell.swift
+//  SnapMap
+//
+//  Created by Lum Situ on 4/12/17.
+//  Copyright © 2017 Jason Wong. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
